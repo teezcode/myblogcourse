6c17d202ecb4118ea11a3afa2abc7521bba0457c
teezcode <spaschal43@gmail.com>
2020-07-16 17:26:07 +0100

    First commit

---
+++ blog/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from blog.models import BlogPost
+
+admin.site.register(BlogPost)
+# Register your models here.
---
+++ personal/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# # Create your models here.
+
+# PRIORITY=[
+#     ("L","Low"),
+#     ("M","Medium"),
+#     ("H","High"),
+
+#     ]
+
+# class Question(models.Model):
+#     title                   =models.CharField(max_length=60)
+#     questions               =models.TextField(max_length=400)
+#     priority                =models.CharField(max_length=1,choices=PRIORITY)  
+
+#     def __str__(self):
+#         return self.title 
+    
+#     class Meta:
+#         verbose_name="The question"
+#         verbose_name_plural="Peoples Question"
